08dfd7b5d22f7f501c7807239420a4c246178ab6
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-27 21:37:38 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def isRational( p, q ) :
+  # Your code goes here
+  
+print("The statement 3/4 is rational is", isRational(3,4))
+print("The statement 4/4 is rational is", isRational(4,4))
+print("The statement 4/3 is rational is", isRational(4,3))
+print("The statement 4/0 is rational is", isRational(4,0))
+print("The statement 0/4 is rational is", isRational(0,4))
